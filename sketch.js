7fedd80e96782a86554b74e84dac62c0d9bf499a
samblaeser <sblaes1@lsu.edu>
2019-09-18 23:59:04 -0500

    changed size

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+  
+    createCanvas(1200, 800);
         
 var xD = 1
 var yD = 12 
@@ -10,8 +11,10 @@ var y = 0
    
     for (var xD = 1; xD < width; xD += 20){
       for (var yD = 12; yD < height; yD += 20){
-         stroke(220, 170, 0, 80);
-        strokeWeight(8);
+    
+     noFill();
+     stroke(220, 170, 0, 80);
+     strokeWeight(8);
      triangle(xD/.25,yD*2, xD, yD, xD, yD);
      } 
      }
@@ -24,38 +27,61 @@ var y = 0
       strokeWeight(2);
       rect(x, y*1.2, x+2, y+2, 20);
     }
-  }
+    }
+
+
   for (var x = 10; x < 1100; x = x + 30){
     for (var y = 0; y < 800; y = y + 40){
    
-  fill(255, 0, 255, 50); 
+      fill(255, 0, 255, 50); 
       stroke(200, 100 ,200)
       strokeWeight(2);
       ellipse(x*6, y*6, x + 15, y + 175);
     }
-  }
+    } 
+
+    for (var x = 10; x < 1100; x = x + 30){
+      for (var y = 0; y < 800; y = y + 40){
+     
+        fill(0, 255, 255, 40); 
+        stroke(200, 100 ,200)
+        strokeWeight(2);
+        ellipse(x*6, y*6, x + 45, y + 205);
+      }
+      } 
+
+      for (var x = 10; x < 1100; x = x + 30){
+        for (var y = 0; y < 800; y = y + 40){
+       
+          fill(255,255, 255, 30); 
+          stroke(200, 100 ,200)
+          strokeWeight(2);
+          ellipse(x*6, y*6, x + 85, y + 245);
+        }
+        } 
+
+
   for (var x = 10; x < 1100; x = x + 30){
     for (var y = 0; y < 800; y = y + 40){
      
       fill(11, 250, 41, 30) 
       stroke(100, 100, 100, 40)
       strokeWeight(2);
-      triangle(x*5, y*5, x + 70, y + 70, x + 40, y + 36);    
-
+      triangle(x*5, y*5, x + 70, y + 70, x + 80, y + 36);    
     }
-  }
+    }
 
 
 fill(random(255),random(255),random(255));
 stroke(random(255),random(255),random(255));
 strokeWeight(2);
 beginShape();
-vertex(70, 40);
-vertex(80, 30);
-vertex(100, 40);
-vertex(120, 30);
-vertex(130, 40);
-vertex(100, 100);
+vertex(5, 20);
+vertex(15, 10);
+vertex(35, 20);
+vertex(55, 10);
+vertex(65, 20);
+vertex(35, 80);
 endShape(CLOSE);
 
 
@@ -77,7 +103,7 @@ circle(random(width*8), random(height/8), random(width/20));
 
  stroke(random(255),random(255),random(255));
  strokeWeight(6);
- point(1710, yS);
+ point(910, yS);
 
 
  yS = yS - 20;
@@ -89,13 +115,14 @@ fill(random(255),random(255),random(255));
 stroke(random(255),random(255),random(255));
 strokeWeight(2);
 beginShape();
-vertex(1740, 760);
-vertex(1730, 770);
-vertex(1710, 760);
-vertex(1690, 770);
-vertex(1680, 760);
-vertex(1710, 700);
+vertex(940, 760);
+vertex(930, 770);
+vertex(910, 760);
+vertex(890, 770);
+vertex(880, 760);
+vertex(910, 700);
 endShape(CLOSE);
 
 
 }
+
